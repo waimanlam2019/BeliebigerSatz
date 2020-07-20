87164e0d21a9a206a88b2b6b210f1286fce500f5
Ray Lam <gunbuster363@gmail.com>
2020-07-20 10:36:20 +0800

    latest

--- src/main/java/me/rayentwickler/model/SimpleSentence1.java
+++ src/main/java/me/rayentwickler/model/SimpleSentence1.java
@@ -1,48 +1,44 @@
 package me.rayentwickler.model;
 
 public class SimpleSentence1 {
-	private Noun subject;
-	private Verb verb;
-	private Noun object;
+	private String subject;
+	private String verb;
+	private String object;
 
 	@Override
 	public String toString() {
 		return subject + " " + verb + " " + object + ".";
 	}
 
-	public SimpleSentence1(Noun subject, Verb verb, Noun object) {
+	public SimpleSentence1(String subject, String verb, String object) {
 		super();
 		this.subject = subject;
 		this.verb = verb;
 		this.object = object;
 	}
 
-	public Noun getSubject() {
+	public String getSubject() {
 		return subject;
 	}
 
-	public void setSubject(Noun subject) {
+	public void setSubject(String subject) {
 		this.subject = subject;
 	}
 
-	public Verb getVerb() {
+	public String getVerb() {
 		return verb;
 	}
 
-	public void setVerb(Verb verb) {
+	public void setVerb(String verb) {
 		this.verb = verb;
 	}
 
-	public Noun getObject() {
+	public String getObject() {
 		return object;
 	}
 
-	public void setObject(Noun object) {
+	public void setObject(String object) {
 		this.object = object;
-	}
-
-	public SimpleSentence1() {
-		// TODO Auto-generated constructor stub
 	}
 
 }
--- src/main/java/me/rayentwickler/web/BeliebigerSatzController.java
+++ src/main/java/me/rayentwickler/web/BeliebigerSatzController.java
@@ -1,6 +1,9 @@
 package me.rayentwickler.web;
 
+import java.util.Random;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,9 +15,23 @@ public class BeliebigerSatzController {
 	@Autowired
 	private RandomSentenceService randomSentenceService;
 
+	@CrossOrigin
 	@GetMapping("/sentence")
 	public SimpleSentence1 simpleSentence1() {
-		System.out.println("Garbage....................");
-		return randomSentenceService.getSimpleSentence();
+
+		Random rand = new Random();
+		int randInteger = rand.nextInt(2);
+		System.out.println(randInteger);
+
+		SimpleSentence1 sentence = null;
+		if (randInteger == 0) {
+			sentence = randomSentenceService.getSimpleSentence();
+			System.out.println(sentence.toString());
+		} else if (randInteger == 1) {
+			sentence = randomSentenceService.getSimpleSentenceWithEin();
+			System.out.println(sentence.toString());
+		}
+
+		return sentence;
 	}
 }
